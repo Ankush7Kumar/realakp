7a751e49be9d21edf7ef916d7cb8aeda96ae3c9a
Ankush Chhokar <ac@eduroam-campus-10-36-223-227.campus-dynamic.uwaterloo.ca>
2025-07-24 09:51:49 -0400

    Link Preview photo added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         path={home.path}
         title={home.title}
         description={home.description}
-        image={`/api/og/generate?title=${encodeURIComponent(home.title)}`}
+        image={`${baseURL}/preview.png`}
         author={{
           name: person.name,
           url: `${baseURL}${about.path}`,
